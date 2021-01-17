9b638b2450cc29a724dd120d98072b155ddb9c37
Alex <cigan@ua.fm>
2021-01-17 16:16:17 +0200

    Add a custom handler for clicking on <h2>

--- react-guide/src/Person/Person.js
+++ react-guide/src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => {
     return (
         <div>
-            <h2>I'm {props.name}! And I'm {props.age} years old!</h2>
+            <h2 onClick={props.clickH2}>I'm {props.name}! And I'm {props.age} years old!</h2>
             <p>{props.children}</p>
         </div>
     );
