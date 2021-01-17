69048083e1238d8f8eaf27e2c8290e862d8e86df
Alex <cigan@ua.fm>
2021-01-17 18:08:44 +0200

    Add style with a separate css file and with an inline style (scoped style)

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -33,10 +33,21 @@ class App extends Component {
     }
 
     render() {
+        const style = {
+            backgroundColor: '#efbd93',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
+
         return (
             <div className="App" >
                 <h1>Hi, I'm a React App</h1>
-                <button onClick={this.switchNameHandler.bind(this, 'Jex')}>Switch a Name</button>
+                <button
+                    onClick={this.switchNameHandler.bind(this, 'Jex')}
+                    style={style}
+                >Switch a Name</button>
                 <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
                 <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changeInput={this.inputChangeHandler}> My hobie is programing</Person>
                 <Person name={this.state.persons[2].name} age={this.state.persons[2].age} clickH2={this.switchNameHandler.bind(this, 'Drue')} />
--- react-guide/src/Person/Person.js
+++ react-guide/src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Person.css'
 
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <h2 onClick={props.clickH2}>I'm {props.name}! And I'm {props.age} years old!</h2>
             <p>{props.children}</p>
             <input type="text" onChange={props.changeInput} value={props.name} />
