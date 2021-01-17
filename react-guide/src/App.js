ce7257a759fd8caa6268f7fa57cee88362c06d88
Alex <cigan@ua.fm>
2021-01-17 16:28:05 +0200

    Add an argument to the method of changing state and bind it with (this, 'arg') to JSX

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -12,12 +12,12 @@ class App extends Component {
         ]
     };
 
-    switchNameHandler = () => {
+    switchNameHandler = (newName) => {
         this.setState({
             persons: [
                 {name: 'Alexander', age: 35},
                 {name: 'Maxim', age: 35},
-                {name: 'Julia', age: 32},
+                {name: newName, age: 32},
             ]
         });
     }
@@ -25,10 +25,10 @@ class App extends Component {
         return (
             <div className="App" >
                 <h1>Hi, I'm a React App</h1>
-                <button onClick={this.switchNameHandler}>Switch a Name</button>
+                <button onClick={this.switchNameHandler.bind(this, 'Jex')}>Switch a Name</button>
                 <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age} clickH2={this.switchNameHandler}> My hobie is programing</Person>
-                <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age} clickH2={this.switchNameHandler.bind(this, 'Koko')}> My hobie is programing</Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age} clickH2={this.switchNameHandler.bind(this, 'Drue')} />
 
             </div>
         )
